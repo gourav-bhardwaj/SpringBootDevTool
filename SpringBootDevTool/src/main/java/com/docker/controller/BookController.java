3d2c36c147a434b87ba019c5af42591f0ed2ae33
Gourav <gov.kumarbharatdwaj@gmail.com>
2020-11-07 08:56:57 +0530

    Modified BookController.java

--- SpringBootDevTool/src/main/java/com/docker/controller/BookController.java
+++ SpringBootDevTool/src/main/java/com/docker/controller/BookController.java
@@ -26,7 +26,7 @@ public class BookController {
 	@Autowired
 	private IBookService BookService;
 
-	@PostMapping("/demoSave")
+	@PostMapping("/demosave")
 	public ResponseEntity<Map<String, Object>> demoSave(@RequestBody Book book) {
 		Map<String, Object> body = new HashMap<String, Object>();
 		body.put("timestamp", LocalDateTime.now());
